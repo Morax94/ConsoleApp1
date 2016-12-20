f46e2e3cb9eb2b91d2f187321cbbaa7be436db55
Przemysław Potaczek <potaczek2626@gmail.com>
2016-12-20 14:10:40 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args[0] == null)
+        {
+            System.Console.Write("Brak parametru");
+            Environment.Exit(0);
+        }
+
+    }
+}
